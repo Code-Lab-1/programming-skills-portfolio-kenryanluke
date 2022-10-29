ddb33b9ea214f6fe20ca86d5e8e3a7412d9a5edd
Kenryan Luke <kenrlbarias@gmail.com>
2022-10-29 16:04:02 +0400

    Chapter 6 answers

---
+++ Chapter 6- Loops/Answers/exercise 4.py
@@ -0,0 +1,11 @@
+sandwich_orders = ["Cheese pimiento", "Grilled Cheese", "Tuna Sandwich"]
+finished_sandwiches = []
+
+while sandwich_orders:
+    sandwich_popped= sandwich_orders.pop()
+    print("I'm working on your " + sandwich_popped + " sandwich.")
+    finished_sandwiches.append(current_sandwich)
+
+print("\n")
+for sandwich in finished_sandwiches:
+    print("I made a " + sandwich + " sandwich.")
